0cca4404a64e940f59d6e0aaa0a745d77bba0035
Araw <markistan123@gmail.com>
2024-04-02 15:15:56 +0800

    123123
    1231231

--- 3d game/Assets/Scripts/pause.cs
+++ 3d game/Assets/Scripts/pause.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Cinemachine;
 
 public class pause : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject PauseMenuUI;
+    public CinemachineVirtualCamera camera;
 
     // Update is called once per frame
     void Update()
@@ -28,6 +30,7 @@ public class pause : MonoBehaviour
     void Resume()
     { 
         PauseMenuUI.SetActive(false);
+        camera.enabled = true;
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
@@ -35,6 +38,7 @@ public class pause : MonoBehaviour
     void Pause()
     {
         PauseMenuUI.SetActive(true);
+        camera.enabled = false;
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
